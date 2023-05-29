a9335d96e493254eb40ad2db97ecef3425b1a247
Beyza Tuna <54098270+beyzatuna@users.noreply.github.com>
2023-05-29 19:46:15 +0300

    Update README.m

--- README.m
+++ README.m
@@ -9,11 +9,14 @@ Framework: TestNG
 Prerequisites
 Before running this project, make sure you have the following prerequisites set up:
 
-Install JDK 8 or any LTS version.
-Configure the JAVA_HOME and GRADLE_HOME environment variables.
+Install JDK 11 or any LTS version.
+Configure the JAVA_HOME, M2_HOME and MAVEN_HOME environment variables.
 Download the latest version of Allure and configure the system environment path.
 Ensure a stable internet connection.
 How to Run This Project
 To run this project, follow these steps:
+Clone the repo in your local directory
+Open terminal to the project folder
+Run command maven clean test to build the project:
+mvn clean test
 
-Clone the repository to your local directory:
